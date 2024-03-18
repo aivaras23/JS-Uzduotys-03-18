8b9cda115b2d084f3132a559a90aa12cd37b3e8d
aivaras23 <aivaraspal1@gmail.com>
2024-03-18 20:04:33 +0200

    fixed

--- uzduotys1.js
+++ uzduotys1.js
@@ -11,6 +11,5 @@ const c = Number(process.argv[4]);
 
 const s = (a + b + c) / 2;
 const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
-const width = (2 * area) / b;
 
-console.log(+width.toFixed(3));
+console.log(+area.toFixed(3));
